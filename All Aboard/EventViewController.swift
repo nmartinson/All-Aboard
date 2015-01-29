5f12dd266c44566a9b664224f6c67e378c603045
Ian <ian@Ians-MacBook-Air.local>
2015-01-29 17:11:24 -0600

    Added the commit page and set the controller for it

---
+++ All Aboard/EventViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  All Aboard
+//
+//  Created by Nick Martinson on 1/28/15.
+//  Copyright (c) 2015 Nick Martinson. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+    
+
+}
