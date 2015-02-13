da182b646d8a3abf90470d99fee446f5f069ebf6
ibrauer <ian-brauer@uiowa.edu>
2015-02-13 10:20:13 -0600

    got the Google Places api integrated into the app so it returns a son object. Also got the date picker (probably) working

--- All Aboard/CreateEventViewController.swift
+++ All Aboard/CreateEventViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 import Alamofire
 
-class CreateEventViewController: UIViewController {
+
+class CreateEventViewController: UIViewController, UITextFieldDelegate {
     var eventType = 0
     @IBAction func eventTypeOption(sender: UISegmentedControl) {
         eventType = sender.selectedSegmentIndex
@@ -20,21 +21,32 @@ class CreateEventViewController: UIViewController {
     @IBOutlet weak var eventNameTextField: UITextField!
     @IBOutlet weak var eventlocationTextField: UITextField!
     @IBOutlet weak var eventDate: UIDatePicker!
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        Alamofire.request(.GET,"https://maps.googleapis.com/maps/api/place/autocomplete/json?key=\(APIkeys.googlePlacesKey)&input=\(eventlocationTextField.text!)&location=41.6667,91.5333", parameters: nil ).responseJSON { (_, response, string,_) -> Void in
+            println("response:\(string)")
+        }
+        return true
+    }
     /*
     title, host, lat, lon, startTime, endTime
 
     */
     @IBAction func onAddFriendsPress(sender: AnyObject) {
+        
+        println(eventType)
         var event = Event(name: eventNameTextField.text ,location: eventlocationTextField.text, date: eventDate.date)
         tf.text = event.EventName
         loc.text = event.EventLocation
+        
         let hostId = UserPreferences().getGUID()
-        let date = NSDate()
-       // let formatter = NSDateFormatter()
-       // formatter.dateFormat = "yyyy-MM-dd-HH-mm"
-       // let time = formatter.stringFromDate(date)
+        
+        
+        let date = eventDate.date
         let timestamp = (date.timeIntervalSince1970) * 1000
         let timestampInMs = Int(timestamp)
+        
+        
         Alamofire.request(.POST,"http://hangout.mybluemix.net/NewEvent", parameters: ["title":eventNameTextField.text, "host":hostId,"lat":41.667,"lon":91.533,"startTime":timestampInMs,"endTime":timestampInMs] ).responseString { (_, response, string,_) -> Void in
             println("response:\(string)")
         }
