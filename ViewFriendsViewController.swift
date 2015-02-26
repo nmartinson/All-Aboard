b4eaca4c85b3224b3fe8a27f1caac579e2193a82
ibrauer <ian-brauer@uiowa.edu>
2015-02-26 17:04:52 -0600

    put a table view in, broken so not sure if it works but I'm pulling master to change

--- ViewFriendsViewController.swift
+++ ViewFriendsViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class ViewFriendsViewController: UIViewController {
     
+    @IBOutlet weak var friendsTableView: UITableView!
+    
     var friendsL:[Dictionary<String,AnyObject>?] = []
 
     override func viewDidLoad() {
@@ -22,5 +24,30 @@ class ViewFriendsViewController: UIViewController {
         
         println(self.friendsL)
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        //creating cell for table view.
+        let cell = self.friendsTableView.dequeueReusableCellWithIdentifier("friendCell", forIndexPath: indexPath) as UITableViewCell
+        
+        var friend:Dictionary<String,AnyObject>
+        
+        //counting how many candies appear in index path
+        friend = self.friendsL[indexPath.row]!
+        
+        //adds label to the cell
+        cell.textLabel?.text = friend["username"]?.value
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friendsL.count
+    }
+
     
 }
