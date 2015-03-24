1bcede7afc76c85d34f76f5955a97b1ecefcee24
Nick Martinson <nick-martinson@uiowa.edu>
2015-03-24 17:17:01 -0500

    Adding the amazon web services

--- All Aboard/ActionCodes.swift
+++ All Aboard/ActionCodes.swift
@@ -10,17 +10,25 @@ import Foundation
 
 enum ACTIONCODES
 {
+    // user
     static let NEW_USER = "110"
     static let UPDATE_USER_PASSWORD = "105"
     static let USER_LOGIN = "100"
     static let GET_USER_INFO = "111"
-    static let NEW_EVENT = "120"
-    static let USER_EVENT_LIST = "124"
+    static let GET_USER_INVITES = "124"
+    static let UPDATE_PASSWORD = "105"
     
+    // Event codes
+    static let NEW_EVENT = "120"
     static let GET_EVENT_INFO = "130"
     static let GET_RECENT_EVENTS = "131"
     static let ADD_FRIEND = "112"
     static let REMOVE_FRIEND = "113"
     static let SEARCH_BY_USERNAME = "140"
     static let GET_USER_FRIEND_LIST = "114"
+    
+    // Invites
+    static let INVITE_USER = "123"
+    static let ACCEPT_INVITE = "125"
+    static let DECLINE_INVITE = "126"
 }
